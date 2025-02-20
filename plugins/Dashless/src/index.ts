71a0d3acf0fadcebc583f6d5e7c800294dd28927
Tiny Games <112739349+TinyGamesCoding@users.noreply.github.com>
2025-02-20 00:01:39 -0500

    add 2 plugins

---
+++ plugins/Dashless/src/index.ts
@@ -0,0 +1,22 @@
+// https://github.com/ssense1337/enmity-plugins/blob/main/Dashless/src/index.tsx
+import { ReactNative } from "@vendetta/metro/common"
+import { after } from "@vendetta/patcher"
+import { findInReactTree } from "@vendetta/utils"
+
+const { View } = ReactNative
+
+const unpatch = after("render", View, (_, res) => {
+    const textChannel = findInReactTree(res, r => r?.props?.channel?.name)
+    if (!textChannel) return
+
+    after("type", textChannel.type, (_, res) => {
+        const textChannelName = findInReactTree(res, r => typeof r?.children === "string")
+        if (!textChannelName) return
+        textChannelName.children = "ae"
+       // textChannelName.children = textChannelName.children.replace(/-/g, " ")
+        return res
+    })
+    unpatch()
+})
+
+export const onUnload = () => unpatch()
